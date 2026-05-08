eaff5e5399e4420f292e2cceda5b964939d32db7
1Daniel1i412 <agonod20@gmail.com>
2026-05-08 11:51:46 -0700

    Fix image paths and remove duplicates

--- kaleo/vite.config.js
+++ kaleo/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Kaleo-Living/', // repo name with slashes
+  base: '/Kaleo-Living/',
+  build: {
+    assetsDir: 'assets', 
+  }
 })
